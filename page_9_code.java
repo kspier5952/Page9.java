3275b690d53dd8634efbc73e0702097e780ababb
kspier5952 <159089040+kspier5952@users.noreply.github.com>
2024-02-07 14:39:17 -0500

    Wrote code for page 9
    Created file page_9_code, and wrote in necessary code.

---
+++ page_9_code.java
@@ -0,0 +1,18 @@
+/**
+ * Project: Page 9
+ * Programmer: Katelyn Spier
+ * Date: February 7, 2024
+ * Program Name: page_9_code.java
+ */
+
+ public class page_9_code
+ {
+     public static void main (String[] args)
+     {
+         System.out.println("Hi"); //message to user
+         System.out.println("LabOneA"); //assignment
+     }
+    }
+    /* This style of commenting is used if a 
+    detailed comment wraps around the screen. 
+    This is block style commenting. */
